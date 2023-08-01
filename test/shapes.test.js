4078f0b76c7f5af68741526b8542829bf0abc8b9
Jiyoh Macbook <geovannih514@gmail.com>
2023-07-31 17:57:59 -0700

    revised tests but having issues passing

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -4,38 +4,38 @@ const { Triangle, Circle, Square, Shapes } = require('../lib/shapes.js') // impo
 describe('Shapes', () => {
   it('should render the complete SVG string', () => {
     const shapes = new Shapes('Triangle', 'Blue', 'OOO')
-    const result = shapes.renderSvg(`<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-    <polygon points="125,30 50,200 200,200" fill="Blue"/>
-    <text text-anchor="middle" x="115" y="180" font-weight="bold" font-size=28px>OOO</text>
-    </svg>`)
+    const result = shapes.renderSvg()
 
-    expect(result).toEqual(true);
-  })
+    expect(result).toEqual('<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"300\" height=\"200\">' +
+    '<polygon points=\"125,30 50,200 200,200\" fill=\"Blue\"/>' +
+    '<text text-anchor=\"middle\" x=\"115\" y=\"180\" font-weight=\"bold\" font-size=\"28px\">OOO</text>' +
+    '</svg>')
+  });
 
     describe('Triangle', () => {
         it('should render the SVG string for a Triangle', () => {
             const triangle = new Triangle('Red')
-            const result = triangle.renderShape(`<polygon points="125,30 50,200 200,200" fill="Red"/>`)
+            const result = triangle.renderShape()
 
-            expect(result).toEqual(true);
+            expect(result).toEqual('<polygon points=\"125,30 50,200 200,200\" fill=\"Red\"/>');
         })
-    })
+    });
 
     describe('Circle', () => {
         it('should render the SVG string for a Circle', () => {
             const circle = new Circle('Green')
-            const result = circle.renderShape(`<circle cx="55" cy="75" r="50" fill="Green"/>`)
+            const result = circle.renderShape()
 
-            expect(result).toEqual(true);
+            expect(result).toEqual('<circle cx=\"55\" cy=\"75\" r=\"50\" fill=\"Green\"/>');
         })
-    })
+    });
 
     describe('Square', () => {
         it('should render the SVG string for a Square', () => {
             const square = new Square('Yellow')
-            const result = square.renderShape(`<rect x="100" y="10" width="300" height="200" fill="Yellowno"/>`)
+            const result = square.renderShape()
 
-            expect(result).toEqual(true);
+            expect(result).toEqual('<rect x=\"100\" y=\"10\" width=\"300\" height=\"200\" fill=\"Yellow\"/>');
         })
-    })
-})
+    });
+});
