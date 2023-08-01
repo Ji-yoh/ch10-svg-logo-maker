d5f3662c83ef1eb4e50a5af80041b94fea583983
Ji-yoh <geovannih514@gmail.com>
2023-08-01 16:55:55 -0700

    added jest test to ensure classes are created and svg strings are created for all shapes, tests are passing

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,41 +1,58 @@
 const { Triangle, Circle, Square, Shapes } = require('../lib/shapes.js') // import shapes classes
 
 // jest tests to test that the SVG code strings are being created
+// added test to check that the classes are being created
+// toBe tests were failing due to incorrect order of constructor parameters..now fixed
 describe('Shapes', () => {
-  it('should render the complete SVG string', () => {
-    const shapes = new Shapes('Triangle', 'Blue', 'OOO')
-    const result = shapes.renderSvg()
+  it('should create a new instance of the Shapes class', () => {
+    const shapes = new Shapes()
+    //const result = shapes.renderSvg()
 
-    expect(result).toEqual('<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"300\" height=\"200\">' +
-    '<polygon points=\"125,30 50,200 200,200\" fill=\"Blue\"/>' +
-    '<text text-anchor=\"middle\" x=\"115\" y=\"180\" font-weight=\"bold\" font-size=\"28px\">OOO</text>' +
-    '</svg>')
+    expect(shapes).toBeInstanceOf(Shapes)
   });
 
     describe('Triangle', () => {
+        it('should create a new instance of the Triangle class', () => {
+            const triangle = new Triangle()
+
+            expect(triangle).toBeInstanceOf(Triangle)
+        })
+
         it('should render the SVG string for a Triangle', () => {
-            const triangle = new Triangle('Red')
+            const triangle = new Triangle('Triangle', 'Red', 'ABC')
             const result = triangle.renderShape()
 
-            expect(result).toEqual('<polygon points=\"125,30 50,200 200,200\" fill=\"Red\"/>');
+            expect(result).toBe("<polygon points=\"125,30 50,200 200,200\" fill=\"Red\"/>");
         })
     });
 
     describe('Circle', () => {
+        it('should create a new instance of the Circle class', () => {
+            const circle = new Circle()
+
+            expect(circle).toBeInstanceOf(Circle)
+        })
+
         it('should render the SVG string for a Circle', () => {
-            const circle = new Circle('Green')
+            const circle = new Circle('Circle', 'Green', 'ABC')
             const result = circle.renderShape()
 
-            expect(result).toEqual('<circle cx=\"55\" cy=\"75\" r=\"50\" fill=\"Green\"/>');
+            expect(result).toBe('<circle cx=\"55\" cy=\"75\" r=\"50\" fill=\"Green\"/>');
         })
     });
 
     describe('Square', () => {
+        it('should create a new instance of the Square class', () => {
+            const square = new Square()
+
+            expect(square).toBeInstanceOf(Square)
+        })
+
         it('should render the SVG string for a Square', () => {
-            const square = new Square('Yellow')
+            const square = new Square('Square', 'Yellow', 'ABC')
             const result = square.renderShape()
 
-            expect(result).toEqual('<rect x=\"100\" y=\"10\" width=\"300\" height=\"200\" fill=\"Yellow\"/>');
+            expect(result).toBe('<rect x=\"100\" y=\"10\" width=\"300\" height=\"200\" fill=\"Yellow\"/>');
         })
     });
 });
